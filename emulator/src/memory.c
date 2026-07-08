02bb877d152c79d85d5bb4a5a6c2f8333c990182
buffersquid <mail@buffersquid.com>
2026-07-07 20:11:28 -0700

    [emulator] Add stack function for push and pop

--- emulator/src/memory.c
+++ emulator/src/memory.c
@@ -22,3 +22,27 @@ word_t alloc(uint32_t n_words) {
   heap_free += n_words;
   return loc;
 }
+
+word_t push_frame(word_t frame_type, word_t saved_env, word_t arg_a,
+                  word_t arg_b, word_t arg_c) {
+  stack_free -= 5;
+  check_memory();
+  memory[stack_free + 0] = frame_type;
+  memory[stack_free + 1] = saved_env;
+  memory[stack_free + 2] = arg_a;
+  memory[stack_free + 3] = arg_b;
+  memory[stack_free + 4] = arg_c;
+  return stack_free;
+}
+
+void pop_frame(word_t *frame_type, word_t *saved_env, word_t *arg_a,
+               word_t *arg_b, word_t *arg_c) {
+  *frame_type = memory[stack_free + 0];
+  *saved_env = memory[stack_free + 1];
+  *arg_a = memory[stack_free + 2];
+  *arg_b = memory[stack_free + 3];
+  *arg_c = memory[stack_free + 4];
+  stack_free += 5;
+}
+
+int stack_empty(void) { return stack_free == MEMORY_SIZE - 1; }
--- emulator/src/memory.h
+++ emulator/src/memory.h
@@ -4,9 +4,18 @@
 
 #define MEMORY_SIZE (1U << (sizeof(word_t) * 8 - 2))
 
+typedef enum { FRAME_EVAL_OP } frame_type_t;
+
 extern word_t memory[MEMORY_SIZE];
 extern word_t heap_free, stack_free;
 
 void mem_init(void);
 word_t alloc(uint32_t n_words); // bump-allocate n words, return start address
+
+word_t push_frame(word_t frame_type, word_t saved_env, word_t arg_a,
+                  word_t arg_b, word_t arg_c);
+void pop_frame(word_t *frame_type, word_t *saved_env, word_t *arg_a,
+               word_t *arg_b, word_t *arg_c);
+int stack_empty(void);
+
 #endif
