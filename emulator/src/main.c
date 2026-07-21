9f2a7b12ad6b12d1667b4a751e4c048eed8fdcc7
buffersquid <mail@buffersquid.com>
2026-07-20 21:34:50 -0700

    [emulator] Modify expression to test all existing primitives

--- emulator/src/main.c
+++ emulator/src/main.c
@@ -17,17 +17,21 @@ int main(int argc, char **argv) {
     printf("[ALERT] running in debug mode\n");
     eval_set_step_hook(debug_step_hook);
   }
-  // (+ 1 (car (cons 2 3)) (+ 4 5 6) 7)
+  // (+ 1 (car (cons 2 3)) (+ 4 5 6) 7 (cdr (cons 66 77)))
   word_t expr0 = cons(
       primitive(CAR_OP),
       cons(cons(primitive(CONS_OP), cons(fixnum(2), cons(fixnum(3), nil()))),
            nil()));
   word_t expr1 = cons(primitive(PLUS_OP),
                       cons(fixnum(4), cons(fixnum(5), cons(fixnum(6), nil()))));
+  word_t expr2 = cons(
+      primitive(CDR_OP),
+      cons(cons(primitive(CONS_OP), cons(fixnum(66), cons(fixnum(77), nil()))),
+           nil()));
   word_t expr =
       cons(primitive(PLUS_OP),
-           cons(fixnum(1), cons(expr0, cons(expr1, cons(fixnum(7), nil())))));
-  // word_t expr = cons(primitive(PLUS_OP), nil());
+           cons(fixnum(1),
+                cons(expr0, cons(expr1, cons(fixnum(7), cons(expr2, nil()))))));
   word_t result = eval_run(expr);
   print_lisp(result);
   printf("\n");
